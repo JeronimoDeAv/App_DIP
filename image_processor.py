2a62950b1ccdbfd5b75c15da4fa05d079d7807a9
Jeronimo Delvasto <jeronimodelvasto16@javerianacali.edu.co>
2024-11-12 12:37:07 -0500

    Update image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -5,25 +5,23 @@ class ImageProcessor:
     def __init__(self, target_size=(128, 128)):
         self.target_size = target_size
 
-    def preprocess_image(self, image_file):
-        # Abrir la imagen y convertir a escala de grises (1 canal)
-        image = Image.open(image_file).convert("L")  # "L" es el modo para escala de grises
-        # Cambiar tamaño a la dimensión esperada
-        image = ImageOps.fit(image, self.target_size, method=Image.Resampling.BILINEAR, bleed=0.0, centering=(0.5, 0.5))
-        # Convertir a array numpy y normalizar
-        img_array = np.array(image) / 255.0
-        # Expandir para añadir dimensión de lote y de canal
-        img_array = np.expand_dims(img_array, axis=0)  # (1, height, width)
-        img_array = np.expand_dims(img_array, axis=-1)  # (1, height, width, 1)
+    def preprocess_image(self, image_file, color_mode="grayscale"):
+        # Abrir la imagen en el modo correspondiente
+        if color_mode == "grayscale":
+            image = Image.open(image_file).convert("L")  # Modo "L" para escala de grises
+            img_array = np.array(image) / 255.0
+            img_array = np.expand_dims(img_array, axis=-1)  # Añadir dimensión de canal
+        elif color_mode == "rgb":
+            image = Image.open(image_file).convert("RGB")  # Modo "RGB"
+            img_array = np.array(image) / 255.0  # Normalizar a [0, 1]
+
+        # Cambiar tamaño y expandir dimensiones
+        image = ImageOps.fit(image, self.target_size, method=Image.Resampling.BILINEAR)
+        img_array = np.expand_dims(img_array, axis=0)  # Añadir dimensión de lote
         return img_array
 
-
     def postprocess_mask(self, mask_array):
-        # Remove the batch dimension
+        # Remover la dimensión de lote y binarizar la máscara
         mask_array = np.squeeze(mask_array, axis=0)
-        # If the mask has multiple channels, pick the channel with the highest probability
-        if mask_array.shape[-1] > 1:
-            mask_array = np.argmax(mask_array, axis=-1)
-        # Convert the mask to binary (0 and 1) based on a threshold, if necessary
-        mask_array = (mask_array > 0.5).astype(np.uint8) * 255  # Convert to 0 and 255 for visualization
+        mask_array = (mask_array > 0.5).astype(np.uint8) * 255
         return mask_array
